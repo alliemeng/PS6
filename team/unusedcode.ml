51135570514b3bd088073b550d86d8b51ba7dcc2
alliemeng <awm79@cornell.edu>
2014-12-03 21:26:22 -0500

    inventorydone

---
+++ team/unusedcode.ml
@@ -0,0 +1,4 @@
+ (* let itemcosts = [cCOST_ETHER; cCOST_MAXPOTION; cCOST_FULLHEAL; 
+    cCOST_REVIVE; cCOST_XATTACK; cCOST_XDEFEND; cCOST_XSPEED]
+  let cheapitem = List.fold_left (fun acc elm -> if elm < acc then elm
+                                else acc) maxint itemcosts *)
