99811700016391ec6be86002598f49152a0b5cc1
Elaine <el532@cornell.edu>
2014-12-04 20:14:34 -0500

    Updated

--- game/game.ml
+++ game/game.ml
@@ -61,7 +61,6 @@ let handle_step (g:game) (ra:command) (ba:command) : game_output =
           handle_SelectStarter g "" blue_starter
       | DoNothing, DoNothing -> handle_SelectStarter g "" ""
       | _,_ -> failwith "Invalid response from bot(s)")
-<<<<<<< HEAD
     | ActionRequest -> (match ra, ba with 
       | Action(SelectStarter starter), DoNothing -> 
         handle_fainted g Red starter
@@ -72,99 +71,6 @@ let handle_step (g:game) (ra:command) (ba:command) : game_output =
      
          
          
-=======
-    | ActionRequest -> 
-        let find_first () : unit =
-          let red_speed = (List.hd g.red.mon_list).speed in
-          let blue_speed = (List.hd g.blue.mon_list).speed in
-          if red_speed > blue_speed then 
-            begin
-              first := Red;
-              Netgraphics.add_update (SetFirstAttacker(Red))
-            end 
-          else if blue_speed > red_speed then 
-            begin
-              first := Blue;
-              Netgraphics.add_update (SetFirstAttacker(Blue))
-            end   
-          (*Pick random team to start*)       
-          else
-            begin
-              if Random.bool () then
-                begin 
-                  first := Red;
-                  Netgraphics.add_update (SetFirstAttacker(Red))
-                end
-              else
-                begin 
-                  first := Blue;
-                  Netgraphics.add_update (SetFirstAttacker(Blue))  
-                end 
-            end in 
-
-        let active_fainted (c:color) : bool = 
-          let active_mon = List.hd (find_player c g).mon_list in 
-          active_mon.curr_hp = 0 in 
-
-   (*      let remove_status_effects (c:color) : unit = 
-          let lst = (find_player c g).mon_list in 
-          match  lst with
-          | h::t -> lst <- {h with status = None} :: t
-          | [] -> failwith "No steammon in team!!" in  *)
-
-        let act (c:color) (action:command) : command option =
-          match action with
-          | Action (SwitchSteammon s) ->
-              switch_steammon g c s;
-              let game_data = game_datafication g in
-              Some(Request(ActionRequest(game_data)))
-
-          | Action (UseItem (item,target)) ->
-              use_item g c item target;
-              let game_data = game_datafication g in
-              if active_fainted c then
-                begin 
-                  (* remove_status_effects c; *)
-                  last_request_sent := StarterRequest;
-                  Some(Request(StarterRequest(game_data)))
-                end
-              else
-                Some(Request(ActionRequest(game_data)))
-
-          | Action (UseMove m) ->
-              use_move g c m;
-              let game_data = game_datafication g in
-              if active_fainted c then
-                begin 
-                  (* remove_status_effects c; *)
-                  last_request_sent := StarterRequest;
-                  Some(Request(StarterRequest(game_data)))
-                end
-              else
-                Some(Request(ActionRequest(game_data)))
-
-          | DoNothing ->
-            let game_data = game_datafication g in 
-            Some(Request(ActionRequest(game_data))) 
-            
-            
-          | _ -> failwith "Invalid bot response" in 
-        send_update (Message ((string_of_color !first)^" goes first"));
-        find_first (); 
-        (*Placeholder, fix later*)
-        if !first = Red then
-            let r = act Red ra in
-            let b = act Blue ba in 
-            (None, game_datafication g, act Red ra, act Blue ra)
-
-        else let b = act Blue ba in let r = act Red ra in 
-        (None, game_datafication g, act Red ra, act Blue ra)
-
-
-
-
-
->>>>>>> e3b9796bb6dfce97e467e0b2cedf5a62b85466c1
 let init_game () : game * request * request * move list * steammon list =
   let s = State.create () in 
 
